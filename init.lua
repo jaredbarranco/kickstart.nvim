3111a8958902d8b2efc773419ed33de236d377a9
barrancojared <51723690+jaredbarranco@users.noreply.github.com>
2024-12-19 11:19:58 -0800

    prettierd: format json files using jq

--- init.lua
+++ init.lua
@@ -652,6 +652,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'prettierd', 'jq', stop_after_first = false },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
